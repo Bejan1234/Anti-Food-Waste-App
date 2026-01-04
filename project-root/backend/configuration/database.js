4a74bfc95c6b77eb806375513c4b1bf7c9948806
Bejan Ionut <ionutbejan678@gmail.com>
2026-01-04 20:08:49 +0200

    fix: enable ssl for supabase

--- project-root/backend/configuration/database.js
+++ project-root/backend/configuration/database.js
@@ -5,9 +5,12 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
-    dialectOptions: {
-        ssl: false
-    },
+    dialectOptions: process.env.NODE_ENV === 'production' ? {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    } : {},
     logging: false // set to true to show sql logs
 });
 
